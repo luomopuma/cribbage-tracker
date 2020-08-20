b7e0685c94943cf960403b699c782823eede3c59
Sam McHaney <luomopuma@gmail.com>
2020-08-20 16:57:41 -0700

    Heyyyy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../layout'
+
 import Contact from "./multipart-file-upload"
 export default function Index() {
   return (
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Layout from '../layout'
+import {Link} from 'gatsby'
 
 export default () => (
   <Layout>
     <h1>Thank you!</h1>
     <p>This is a custom thank you page for form submissions</p>
+    <Link to={"/multipart-file-upload/"}>Home</Link>
   </Layout>
 )
