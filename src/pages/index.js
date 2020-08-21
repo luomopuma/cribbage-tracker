2b42697ddd595a78289e1f9cfe88f6e43f7dff2c
Sam McHaney <luomopuma@gmail.com>
2020-08-20 17:05:37 -0700

    HEYYYYyyyyy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../layout'
-
 import Contact from "./multipart-file-upload"
+
 export default function Index() {
   return (
       <Contact/>
